02620d4fe73cb698b1defddc9fcd12b37903d9e4
maria eduarda <162511576+mariaeduardamoreira22c@users.noreply.github.com>
2024-03-28 18:30:58 -0300

    Add files via upload

---
+++ parte1.sql/trabalho01_bim01..sql
@@ -0,0 +1,31 @@
+create database sistema_vendas_22c;
+use sistema_vendas_22c;
+
+create table cliente(
+nome varchar(100) not null,
+endereco varchar(255) not null,
+email varchar (50) not null,
+celular bigint (11),
+dt_nascimento date not null)
+;
+create database sistema_vendas_22c;
+use sistema_vendas_22c;
+
+create table compras(
+nome varchar(100) not null,
+descricao varchar(255) not null,
+quantidade_em_estoque bigint (100)
+preco bigint (11),
+quantidade em estoque bigint (100)
+);
+
+create database sistema_vendas_22c;
+use sistema_vendas_22c;
+
+create table reazizar pedidos(
+nome varchar(100) not null,
+valor total bigint (15),
+dt_de_compra date not null)
+);
+
+
